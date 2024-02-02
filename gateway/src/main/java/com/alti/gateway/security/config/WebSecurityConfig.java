c8b450630018ce0ea2da46919883bf595f80bd86
NarendraRaju <ynarendraraju@gmail.com>
2024-02-02 09:13:01 +0530

    added sec config

---
+++ gateway/src/main/java/com/alti/gateway/security/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.alti.gateway.security.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+
+public class WebSecurityConfig  {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		 http.csrf().disable()
+         .authorizeHttpRequests()
+         .anyRequest().authenticated()
+         .and()
+         .httpBasic();
+ return http.build();
+		
+	}
+
+}
