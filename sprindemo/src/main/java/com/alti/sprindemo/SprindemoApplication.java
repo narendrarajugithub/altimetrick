ae09bb6197ebbc5278aed4ba7772dd2520f48731
NarendraRaju <ynarendraraju@gmail.com>
2024-02-15 12:22:31 +0530

    commit

--- sprindemo/src/main/java/com/alti/sprindemo/SprindemoApplication.java
+++ sprindemo/src/main/java/com/alti/sprindemo/SprindemoApplication.java
@@ -2,7 +2,8 @@ package com.alti.sprindemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
 @SpringBootApplication
 public class SprindemoApplication {
 
