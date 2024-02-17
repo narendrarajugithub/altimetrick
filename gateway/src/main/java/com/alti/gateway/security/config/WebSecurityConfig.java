377bde5dee0b018a7d8cf3f32d96cb8141a9f657
NarendraRaju <ynarendraraju@gmail.com>
2024-02-17 19:15:22 +0530

    changed

--- gateway/src/main/java/com/alti/gateway/security/config/WebSecurityConfig.java
+++ gateway/src/main/java/com/alti/gateway/security/config/WebSecurityConfig.java
@@ -1,14 +1,7 @@
 package com.alti.gateway.security.config;
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@EnableWebSecurity
-@Configuration
+/* @EnableWebSecurity
+// @Configuration
 
 public class WebSecurityConfig  {
 	
@@ -24,3 +17,4 @@ public class WebSecurityConfig  {
 	}
 
 }
+*/
