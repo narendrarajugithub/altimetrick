7ee0677fe6d9be00d1dce985faf16bef635f27b1
NarendraRaju <ynarendraraju@gmail.com>
2024-02-17 19:14:29 +0530

    sample bean vendor is added

--- gateway/src/main/java/com/alti/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/alti/gateway/GatewayApplication.java
@@ -2,12 +2,8 @@ package com.alti.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
