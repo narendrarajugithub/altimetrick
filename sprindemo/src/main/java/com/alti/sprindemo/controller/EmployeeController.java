e25357d5e1e3f5d9443050eee24522e408500ee5
NarendraRaju <ynarendraraju@gmail.com>
2024-03-21 21:23:24 +0530

    request Parameters added to controller

--- sprindemo/src/main/java/com/alti/sprindemo/controller/EmployeeController.java
+++ sprindemo/src/main/java/com/alti/sprindemo/controller/EmployeeController.java
@@ -1,14 +1,20 @@
 package com.alti.sprindemo.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +23,7 @@ import com.alti.sprindemo.security.config.vendorConfiguration;
 import com.alti.sprindemo.service.EmployeeService;
 
 @RestController
+//@RequestMapping(path = "/employee-services")
 public class EmployeeController {
 	
 	private static Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
@@ -52,5 +59,17 @@ public class EmployeeController {
 		return ResponseEntity.badRequest().build();
 		
 	}	
+	
+	@GetMapping(value = "/employees/{id}")
+	public ResponseEntity<?> getEmployees(@PathVariable List<Long> id ){
+		LOG.info("Employee with Pathparam List executed");
+		return null;
+	}
+	
+	@GetMapping(value = "/employees" , params = "id")
+	public ResponseEntity<?> getEmployeesWithReqParams(@RequestParam List<Long> id ){
+		LOG.info("Employee with Pathparam List executed");
+		return null;
+	}
 
 }
